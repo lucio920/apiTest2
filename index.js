f686a56a69157c574af001fdc4e3d0629c8db740
lucio920 <lucioannese959@gmail.com>
2024-11-11 16:57:30 -0300

    version 1.0

---
+++ index.js
@@ -0,0 +1,88 @@
+import express from 'express';
+import fs from "fs";
+import bodyParser from "body-parser";
+
+/*FUNCIONES*/ 
+
+const app = express();
+app.use(bodyParser.json());
+
+const readData = () => {
+    try {
+        const data = fs.readFileSync("./db.json");
+        return JSON.parse(data);
+    } catch(error){
+        console.log(error);
+    }
+        
+};
+
+const writeData = (data) => {
+    try{
+        fs.writeFileSync("./db.json",JSON.stringify(data));
+    } catch(error){
+        console.log(error);
+    }
+};
+
+
+/*ENDPOINTS*/ 
+
+app.get("/",(req,res) => { //PAGINA PRINCIPAL
+    res.send("Api Test 2");
+});
+
+app.get("/books",(req,res) => { //OBTENER TODOS LOS LIBROS
+    const data=readData();
+    res.json(data.books);
+});
+
+app.get("/books/:id",(req,res)=>{ //BUSCAR LIBRO POR ID
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const book = data.books.find((book) => book.id === id);
+    res.json(book);
+});
+
+app.post("/books",(req,res)=>{ //LIBRO NUEVO
+    const data = readData();
+    const body = req.body;
+    const newBook = {
+        id: data.books.length + 1,
+        ...body,
+    };
+    data.books.push(newBook);
+    writeData(data);
+    res.json(newBook);
+});
+
+app.put("/books/:id",(req,res)=>{ //MODIFICAR LIBRO POR ID
+    const data = readData();
+    const body = req.body;
+    const id = parseInt(req.params.id);
+    const bookIndex = data.books.findIndex((book) => book.id === id);
+    data.books[bookIndex]= {
+        ...data.books[bookIndex],
+        ...body,
+    };
+    writeData(data);
+    res.json({message: "Book Updated"});
+});
+
+app.delete("/books/:id",(req,res)=>{ //BORRAR POR ID
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const bookIndex = data.books.findIndex((book) => book.id === id);
+    data.books.splice(bookIndex,1);
+    writeData(data);
+    res.json({message: "Book Deleted"});
+
+});
+
+
+
+
+/**//**//**//**//**//**//**//**//**//**//**//**/
+app.listen(3000, () => {
+    console.log("Server listening on port 3000");
+});
